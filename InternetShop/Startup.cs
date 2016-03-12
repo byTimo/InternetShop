22acd71ef51c49700bd38aee96a1e2ac992346dd
byTimo <race.timo@gmail.com>
2016-03-12 23:50:43 +0500

    Init commit

---
+++ InternetShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetShop.Startup))]
+namespace InternetShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
