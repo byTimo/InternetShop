83dbfdcc9f0b3d4e4df62f051cf519c30e1d1248
byTimo <race.timo@gmail.com>
2016-03-15 12:44:21 +0500

    Перенёс модальное окно в layout

--- InternetShop.WebUI/Scripts/adminModalInvoker.js
+++ InternetShop.WebUI/Scripts/adminModalInvoker.js
@@ -2,9 +2,6 @@
     $(".edit-product").click(function() {
         var url = "/Admin/EditProduct";
         var id = $(this).attr("data-id");
-        $.get(url + "?productId=" + id, function(data) {
-            $("#edit-product-modal-content").html(data);
-            $("#edit-product-modal").modal("show");
-        });
+        invokeModal(url + "?productId=" + id);
     });
 });
