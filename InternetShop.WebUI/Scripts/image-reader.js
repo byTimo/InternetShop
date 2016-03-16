a288b0009c8e839670e806971d7ca5e2fe6eb3cc
byTimo <race.timo@gmail.com>
2016-03-16 19:37:32 +0500

    Изменил представления для создания редактирования, можно загружать из обоих

---
+++ InternetShop.WebUI/Scripts/image-reader.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+    $("#input-image").bind({
+        change: function() {
+            displayFiles(this.files[0]);
+        }
+    });
+
+    function displayFiles(file) {
+
+        if (!file.type.match(/image.*/)) {
+            return;
+        }
+        var img = $("#image");
+        var reader = new FileReader();
+        reader.onload = (function(aImg) {
+            return function(e) {
+                aImg.attr('src', e.target.result);
+            };
+        })(img);
+        reader.readAsDataURL(file);
+    }
+});
